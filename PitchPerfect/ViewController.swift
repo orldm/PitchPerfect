c85eb9a7327aef3a7fc72a7591cc7a3dbece593c
Dmitri <orldm@mail.ru>
2014-11-25 22:26:53 +0200

    Add button and its asset

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("loaded ok from desktop")
+        //println("loaded ok from desktop")
     }
 
     override func didReceiveMemoryWarning() {
